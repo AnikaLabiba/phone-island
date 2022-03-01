8b6d9239c20dec797794ec799900588bf0a499d7
AnikaLabiba <anikalabibajahin@gmail.com>
2022-03-01 13:26:11 +0600

    phone deails part added

--- js/app.js
+++ js/app.js
@@ -7,12 +7,48 @@ const toggleSearchResult = displayStyle => {
     document.getElementById('search-results').style.display = displayStyle;
 }
 const showError = displayStyle => {
-    document.getElementById('error').style.display = displayStyle;
+    document.getElementById('error-nullArray').style.display = displayStyle;
+}
+// //accessing main features
+const accessMainFeatures = features => {
+    //console.log(features.sensors)
+    const result = Object.values(features)
+
+    const resultProperty = `
+    <p class="card-text"><small class="text-muted">
+    <span class="fw-bold">Storage:</span> ${result[0]}<br>
+    <span class="fw-bold">Display size:</span> ${result[1]}<br>
+    <span class="fw-bold">Chipset:</span> ${result[2]}<br>
+    <span class="fw-bold">Memory:</span> ${result[3]}<br>
+    <span class="fw-bold">Sensors:</span> ${result[4] ? result[4] : 'not found'}</small>
+    </p>`
+
+    return resultProperty
+}
+//access others property
+const accessOthers = others => {
+    console.log(others)
+    const result = Object.values(others)
+
+    const resultProperty = `
+    <p class="card-text"><small class="text-muted">
+    <span class="fw-bold">WLAN:</span> ${result[0]}<br>
+    <span class="fw-bold">Bluetooth:</span> ${result[1]}<br>
+    <span class="fw-bold">GPS:</span> ${result[2]}<br>
+    <span class="fw-bold">NFC:</span> ${result[3]}<br>
+    <span class="fw-bold">Radio:</span> ${result[4]}<br>
+    <span class="fw-bold">USB:</span> ${result[5]}<br>
+    </small>
+    </p>`
+
+    return resultProperty
 }
 
 //taking search value
 const searchPhone = () => {
     const searchText = document.getElementById('search-field').value
+    const phoneDetails = document.getElementById('phone-details')
+    phoneDetails.textContent = ''
     toggleSpinner('block')
     toggleSearchResult('none')
     showError('none')
@@ -21,22 +57,24 @@ const searchPhone = () => {
 }
 //loading phone info by name
 const loadPhones = searchText => {
-    if (searchText == '') {
-        console.log('sorry')
-    }
-    else {
+    if (searchText != '') {
+
         const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
             .then(res => res.json())
-            .then(data => displayPhones(data.data))
+            .then(data => displayPhones(data.data.slice(0, 20)))
     }
-
+    else {
+        toggleSpinner('none')
+        showError('block')
+    }
 }
 //displaying phones
 const displayPhones = phones => {
-    console.log(phones)
     const searchresults = document.getElementById('phones')
     searchresults.textContent = ''
+    const phoneDetails = document.getElementById('phone-details')
+    phoneDetails.textContent = ''
     if (phones.length === 0) {
         showError('block')
         toggleSpinner('none')
@@ -44,7 +82,7 @@ const displayPhones = phones => {
     else {
 
         phones.forEach(phone => {
-            //console.log(phone)
+
             const div = document.createElement('col')
 
             div.innerHTML = `
@@ -54,7 +92,7 @@ const displayPhones = phones => {
                                 <div class="card-body">
                                     <h5 class="card-title">Name: ${phone.phone_name}</h5>
                                     <h6 class="card-text">Brand: ${phone.brand}</h6>
-                                    <a href="#" class="btn btn-outline-primary px-5">Details</a>
+                                    <a onclick="loadDetail('${phone.slug}')" href="#" class="btn btn-outline-primary px-5">Details</a>
                                 </div>
                             </div>
                         </div>
@@ -65,5 +103,39 @@ const displayPhones = phones => {
         toggleSearchResult('block')
         toggleSpinner('none')
     }
+}
+// loading phone details by id
+const loadDetail = id => {
 
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data))
+}
+//display phone details
+const displayPhoneDetail = phone => {
+    console.log(phone)
+    const phoneDetails = document.getElementById('phone-details')
+    phoneDetails.textContent = ''
+    const div = document.createElement('div')
+    div.innerHTML = `
+           <div class="card mb-3 w-75 mx-auto mt-5" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Name: ${phone.name}</h5>
+                            <p class="card-text">Released on: ${phone.releaseDate ? phone.releaseDate : 'not found'}</p>
+                            
+                            <h6>Main features: ${accessMainFeatures(phone.mainFeatures)}</h6>
+                           
+                             <h6>Others: ${accessOthers(phone.others ? phone.others : 'not found')} </h6>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    phoneDetails.appendChild(div)
 }
